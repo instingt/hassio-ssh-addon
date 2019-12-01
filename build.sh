2c2f06ba477d164beeb671d1f94fdd75638d00f6
Vitaliy Kataev <v.v.kataev@tinkoff.ru>
2019-12-01 13:52:39 +0300

    fix build '

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 docker run \
-    --rm --privileged \
-    -v ~/.docker:/root/.docker \
+    -it --rm --privileged \
     homeassistant/amd64-builder \
+    --docker-login \
     --aarch64 \
     --target addon \
     -r https://github.com/instingt/hassio-ssh-addon \
-    -b master
+    -b master \
